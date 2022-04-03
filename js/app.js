3b7f5e8ade6678f7b910cc469e15778e8c7be447
DaianaArena <84255114+DaianaArena@users.noreply.github.com>
2022-04-03 05:00:55 -0300

    adding shopping cart

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
+//1: hacer un evento click en cualquier objeto del carrito (CUIDADO PORQUE LOS TR NO SE CREAN HASTA LA FUNCION) con un if target= boton + o boton -
+
 const templateCard = document.getElementById('template-card').content;
 const varitasContainer = document.getElementById('varitasContainer');
 const fragment = document.createDocumentFragment();
+const templateCart = document.getElementById('template-cart').content;
+const cartContainer = document.getElementById('cartContainer');
+
+let cart = {};
 
 document.addEventListener('DOMContentLoaded', () => {
   fetchProducts();
 
 });
+
+varitasContainer.addEventListener('click', (event) => {
+  addToCart(event);
+});
+
 
 
 const fetchProducts = async () => {
@@ -24,12 +35,13 @@ const fetchProducts = async () => {
 function displayProducts  (products) {
   products.forEach(product => {
 
-    console.log(product.name, product.id, product.description, product.image, product.house, product.price);
+
     templateCard.querySelector('h3').textContent = product.name;
     templateCard.querySelector('img').src = product.image;
     templateCard.querySelector('p').textContent = product.description;
     templateCard.querySelector('h4').textContent = `$`+product.price;
     templateCard.querySelector('span').textContent = product.house;
+    templateCard.querySelector('button').setAttribute('data-id', product.id);
 
 
     //Elegir el color del badge dependiendo de la casa
@@ -52,9 +64,61 @@ function displayProducts  (products) {
     }
 
 
-    const newCard = templateCard.cloneNode(true);
+    let newCard = templateCard.cloneNode(true);
     fragment.appendChild(newCard);
   });
 
   varitasContainer.appendChild(fragment);
 }
+
+const addToCart = (event) => {
+
+
+ if (event.target.classList.contains("btn-primary")) {
+    setCart(event.target.parentElement);
+
+  }
+  event.stopPropagation();
+}
+
+const setCart = object => {
+  const product = {
+    id: object.querySelector('button').getAttribute('data-id'),
+    name: object.querySelector('h3').textContent,
+    price: object.querySelector('h4').textContent,
+    quantity: 1
+
+  }
+
+  if  (cart.hasOwnProperty(product.id)) {
+    cart[product.id].quantity++;
+
+
+  } else {
+    cart[product.id] = product;
+  }
+
+  displayCart(cart);
+}
+
+function displayCart(cart) {
+
+  cartContainer.innerHTML = '';
+  for (let key in cart) {
+    let product = cart[key];
+    templateCart.querySelector("tr").setAttribute('data-id', product.id);
+    templateCart.getElementById('id').textContent = product.id;
+    templateCart.getElementById('nombre').textContent = product.name;
+    templateCart.querySelector('span').textContent = product.quantity;
+    templateCart.getElementById('precio').textContent = product.price;
+    templateCart.getElementById('total').textContent = `$`+ (product.quantity * parseInt(product.price.substring(1), 10));
+
+    let addProductToCart = templateCart.cloneNode(true);
+    fragment.appendChild(addProductToCart);
+    cartContainer.appendChild(fragment);
+  }
+
+
+}
+
+
